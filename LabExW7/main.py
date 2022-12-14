ba52827bfdc200c912785bb2a0356821ec2ae0c7
Egehan Ceylan <cegehan.co@gmail.com>
2022-12-14 14:47:20 +0300

    Update main.py

--- LabExW7/main.py
+++ LabExW7/main.py
@@ -40,10 +40,8 @@ def dice_sum():
 
 dice_sum()
 
+
 # ### Exercise 3
-
-prime_factors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
 
 def findLCM(n1, n2):
     def gcd(a, b):
